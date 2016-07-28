a4a310c79f47651db880cb478a336e534fbd848a
meenalw <meenal.walokar@gmail.com>
2016-07-28 15:51:04 +0530

    initial commit

---
+++ src/main/java/basic/Contact.java
@@ -0,0 +1,60 @@
+/**
+ * @filenameName:basic.Contact.java
+ * @description:TODO
+ * @author meenalw
+ * @date Jul 26, 2016 3:36:17 PM
+ * @version: TODO
+ */
+
+package basic;
+
+/**
+ * @className:basic.Contact.java
+ * @description:TODO
+ * @author meenalw
+ * @date Jul 26, 2016 3:36:17 PM
+ */
+
+public class Contact {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private long id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
